18f6ba70b2888eef581837104132152cf3ec8b37
Sourav <souravroy472@gmail.com>
2016-08-29 16:55:55 +0530

    Kadane's algo with index of starting and ending subarray

--- ccodes/KadaneAlgo.cpp
+++ ccodes/KadaneAlgo.cpp
@@ -8,26 +8,59 @@ using namespace std;
 
 int kadane(int a[], int n)
 {
-	int curr_max = 0, max_sum = a[0], i, start = 0, end = 0, s = 0;
+	int curr_max = 0, max_sum = a[0], i, start = 0, end = 0, s = 0, maxi = INT_MIN, flag = 0;
 	for(i = 0;i < n;i++)
 	{
-		curr_max += a[i];
-		if(curr_max < 0)
+		if(a[i] > 0)
 		{
-			if(i + 1 < n)
-			start = i + 1;
-			curr_max = 0;
-		}	
+			
+			flag = 1;
+			break;
 
-		else if(curr_max > max_sum)
-		{
-			s = start;
-			end = i;
-			max_sum = curr_max;
 		}
+			
+
+		else
+		{
+			if(maxi < a[i])
+			{
+				s = i;
+				end = i;
+				maxi = a[i];
+			}
+		}
+
+
 	}
 
-	cout << "Start : " << s << " End : " << end << " Max sum : " << max_sum << endl;
+	if(!flag && maxi < 0)
+	{
+		cout << "Start : " << s << " End : " << end << " Max sum : " << maxi << endl;
+	}
+	if(flag)
+	{
+		for(i = 0;i < n;i++)
+		{
+			curr_max += a[i];
+			if(curr_max < 0)
+			{
+				if(i + 1 < n)
+				start = i + 1;
+				curr_max = 0;
+			}	
+
+			else if(curr_max > max_sum)
+			{
+				s = start;
+				end = i;
+				max_sum = curr_max;
+			}
+		}
+
+		cout << "Start : " << s << " End : " << end << " Max sum : " << max_sum << endl;
+	}
+	
+	
 	curr_max = a[0], max_sum = a[0];
 	for(i = 1;i < n;i++)
 	{
@@ -41,7 +74,7 @@ int kadane(int a[], int n)
 
 int main()
 {
-    int a[] = {631, -583, -975};
+    int a[] = {-1, -1, -1};
     int n = sizeof(a)/sizeof(a[0]);
     int max_sum = kadane(a, n);
     cout << "Maximum contiguous sum is : " << max_sum << endl;
